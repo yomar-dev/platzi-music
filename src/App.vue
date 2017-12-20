8dde476608223573deed920371b8a5ab2bd104ba
Yomar Miranda <mirandayomar@gmail.com>
2017-12-19 23:02:18 -0500

    Vimos algunos ejemplos de expresiones.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   #app
     img(src="./assets/logo.png")
     h1 {{ msg }}
+    p {{ 3 * 4 }}
+    p {{ 'Hola '  + 'Mundo!!'}}
+    p {{ person.name + ' ' + person.lastname }}
+    p {{ person.name.toUpperCase() }}
+    p {{ JSON.stringify(person) }}
 </template>
 
 <script>
@@ -9,7 +14,11 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hello World!!'
+      msg: 'Hello World!!',
+      person: {
+        name: 'Xavi',
+        lastname: 'Hernandez'
+      }
     }
   }
 }
