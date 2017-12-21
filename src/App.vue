ece972d51aeb161efc880cb9002aaceb051d2a61
Yomar Miranda <mirandayomar@gmail.com>
2017-12-20 23:33:13 -0500

    Ejemplo de Computed Properties.

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,13 @@
     ul
       li(v-for="i in items") {{ i }}
 
-    input(v-model="name" placeholder="Ingresa tu nombre")
-    p {{ name }}
 
     a(v-bind:href="url" target="_blank") Link
+    br
+
+    input(v-model="name" placeholder="Ingresa tu nombre")
+    input(v-model="lastName" placeholder="Ingresa tu apellido")
+    p {{ fullName }}
 </template>
 
 <script>
@@ -24,7 +27,18 @@ export default {
       value: 'Hello World!!',
       items: [1, 2, 3, 4],
       name: '',
-      url: 'https://platzi.com/@yomar_dev/'
+      url: 'https://platzi.com/@yomar_dev/',
+      lastName: ''
+    }
+  },
+  /**
+   * Computed Properties
+   * Son funciones que devuelven un valor y dentro de estas funciones
+   * puedo utilizar funciones ya existentes.
+   */
+  computed: {
+    fullName () {
+      return this.name + ' ' + this.lastName
     }
   }
 }
